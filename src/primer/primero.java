b6e3767d31ee272104afd66021181e179106af75
CarlosCanoTrivinho <ccanot01@iesarroyoharnina.es>
2024-01-18 09:35:11 +0100

    as

--- src/primer/primero.java
+++ src/primer/primero.java
@@ -1,9 +1,9 @@
 package primer;
 
 public class primero {
-public static void main(String[] args) {
-	int uno=2;
-	System.out.println("Hola");
-	System.out.println("Hola");
-}
+	public static void main(String[] args) {
+		int uno = 2;
+		System.out.println("Hola");
+		System.out.println("Hola");
+	}
 }
